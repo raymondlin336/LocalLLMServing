1098cef7f8ef88a68f63df16ddebd596881b7a5a
shourya <shourya.h.vardhan@gmail.com>
2025-10-09 00:03:25 -0400

    make ollama accesible from anywhere

--- src/HostSide/start-ollama-linux.sh
+++ src/HostSide/start-ollama-linux.sh
@@ -7,7 +7,7 @@ if ss -ltn | grep -q ":$PORT "; then
     ip=$(ss -ltn | awk -v p=":$PORT" '$4 ~ p {print $4}' | head -n1 | cut -d':' -f1)
     echo "Ollama is already launched on $ip:$PORT"
 else
-    export OLLAMA_HOST=127.0.0.1:$PORT
+    export OLLAMA_HOST=0.0.0.0:$PORT
     echo "Starting Ollama on $OLLAMA_HOST..."
     ollama serve
 fi
